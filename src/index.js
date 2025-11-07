359aaee69e61d2eeee537022d7649d3b65e8496b
Your Name <youremail@example.com>
2025-11-07 14:51:30 +0300

    Added BrowserRouter and homepage for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -6,15 +6,13 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { BrowserRouter } from 'react-router-dom'; // <-- import BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
+  <BrowserRouter basename="/burger-delivery-app"> {/* <-- add basename */}
     <App />
- 
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
